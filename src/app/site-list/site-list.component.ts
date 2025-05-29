87e421a909424b7e2d3b09255f452526305f9843
UtsavJ <utsavjaiswal2004@gmail.com>
2025-05-29 23:45:21 +0530

    firebase connection

--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import { PasswordManagerService } from '../password-manager.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-site-list',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './site-list.component.html',
   styleUrl: './site-list.component.css'
 })
 export class SiteListComponent {
+
+  allSites! : Observable <Array<any>>
+  
+  constructor(private passwordManagerService : PasswordManagerService) {
+    this.loadSites();
+  }
+
+
   onSubmit(formValues : object){
     console.log("Submitted");
     console.log(formValues);
-    
+    this.passwordManagerService.addSite(formValues)
+    .then(
+      ()=>{
+        console.log('Data saved');
+      }
+    )
+    .catch(
+      (err)=>{
+        console.log(err);
+      }
+    )
+  }
+
+
+  loadSites(){
+    this.allSites = this.passwordManagerService.loadSites();
   }
 }
+
+        
+       
+        
